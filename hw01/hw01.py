eb54e3632422a5421223ecdadb5fc8072eafc5d0
dundunkirk <787741917@qq.com>
2023-12-24 03:10:03 +0000

    hw01

--- hw01/hw01.py
+++ hw01/hw01.py
@@ -51,6 +51,9 @@ def largest_factor(n):
     1
     """
     "*** YOUR CODE HERE ***"
+    for i in range(n//2, 0, -1):
+        if n % i == 0:
+            return i
 
 
 def if_function(condition, true_result, false_result):
